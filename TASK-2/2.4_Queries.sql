2509cbb6c82be9159d3fb49fc2e9118bbc63049f
Gleb Kharitonov <g.xaritonov@bk.ru>
2023-05-27 12:37:28 +0300

    editted qs

--- TASK-2/2.4_Queries.sql
+++ TASK-2/2.4_Queries.sql
@@ -1,3 +1,4 @@
+BEGIN;
 --1)
 DELETE
 FROM student
@@ -9,6 +10,10 @@ SET date_birth = '01-01-1999'
 WHERE date_birth IS NULL;
 
 --3)
+DELETE
+FROM student_hobby
+WHERE student_id = 21;
+
 DELETE
 FROM student
 WHERE id = 21;
@@ -37,8 +42,8 @@ FROM student_hobby
 WHERE date_finish IS NOT NULL;
 
 --7)
-INSERT INTO student_hobby (student_id, hobby_id, date_start, date_finish)
-VALUES (4, 5, '2009-11-15', null);
+INSERT INTO student_hobby (id, student_id, hobby_id, date_start, date_finish)
+VALUES (nextval('student_hobby_id_seq'), 4, 5, '2009-11-15', null);
 
 --8)
 DELETE
@@ -54,11 +59,12 @@ WHERE id in (SELECT id
                AND sh.date_start = last_times.m_d);
 
 --9)
-INSERT INTO hobby (name, risk)
-VALUES ('бальные танцы', 0.5);
+INSERT INTO hobby (id, name, risk)
+VALUES (nextval('hobby_id_seq'), 'бальные танцы', 0.5);
 
-INSERT INTO hobby (name, risk)
-VALUES ('вышивание крестиком', 0.3);
+
+INSERT INTO hobby (id, name, risk)
+VALUES (nextval('hobby_id_seq'), 'вышивание крестиком', 0.3);
 
 UPDATE student_hobby
 SET hobby_id = CASE
@@ -79,8 +85,6 @@ INSERT INTO hobby (name, risk)
 VALUES ('учёба', 0.2);
 
 --11)
---Можно ли в одном запросе, если нет хобби, то добавить?
-
 UPDATE student_hobby
 SET hobby_id = 11
 WHERE student_id in (select s.id from student s where score < 3.2)
@@ -98,7 +102,11 @@ UPDATE student
 SET n_group = n_group + 1000
 WHERE n_group / 1000 < 4;
 
---13) 0_o -> 3 the same
+--13)
+DELETE
+FROM student_hobby
+WHERE student_id = 2;
+
 DELETE
 FROM student
 WHERE id = 2;
@@ -119,3 +127,4 @@ WHERE student_id IN (SELECT s.id
                      FROM student s
                               JOIN student_hobby sh ON s.id = sh.student_id
                      WHERE sh.date_start < s.date_birth);
+COMMIT;
